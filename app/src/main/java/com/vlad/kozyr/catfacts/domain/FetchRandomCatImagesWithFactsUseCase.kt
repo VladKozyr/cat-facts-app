e8b8e66233759c565386dcac460a4fd17618cb75
VladKozyr <vladyslav.kozyr@gmail.com>
2023-10-22 19:23:06 +0300

    [After DL] Add parallel requests

--- app/src/main/java/com/vlad/kozyr/catfacts/domain/FetchRandomCatImagesWithFactsUseCase.kt
+++ app/src/main/java/com/vlad/kozyr/catfacts/domain/FetchRandomCatImagesWithFactsUseCase.kt
@@ -1,6 +1,8 @@
 package com.vlad.kozyr.catfacts.domain
 
 import com.vlad.kozyr.catfacts.domain.model.CatImageWithFact
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import java.lang.Integer.min
 import javax.inject.Inject
 
@@ -8,12 +10,14 @@ class FetchRandomCatImagesWithFactsUseCase @Inject constructor(
     private val catRepository: CatRepository,
 ) {
 
-    suspend operator fun invoke(): List<CatImageWithFact> {
+    suspend operator fun invoke(): List<CatImageWithFact> = coroutineScope {
+        val imagesDef = async { catRepository.getRandomCatImages(LOAD_SIZE) }
+        val factsDef = async { catRepository.getRandomCatFacts(LOAD_SIZE) }
 
-        val images = catRepository.getRandomCatImages(LOAD_SIZE)
-        val facts = catRepository.getRandomCatFacts(LOAD_SIZE)
+        val images = imagesDef.await()
+        val facts = factsDef.await()
 
-        return List(min(images.size, facts.size)) { index ->
+        List(min(images.size, facts.size)) { index ->
             CatImageWithFact(
                 images[index].url,
                 facts[index].fact
